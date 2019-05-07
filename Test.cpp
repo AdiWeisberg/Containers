e487d32cb86bfca27dde286f9d62d78711500911
Michal Yelkin <44944939+michalkin9@users.noreply.github.com>
2019-05-07 17:58:33 +0300

    test tamplate

--- Test.cpp
+++ Test.cpp
@@ -1,1 +1,37 @@
 //tests
+#include <iostream>
+using namespace std;
+
+#include "zip.hpp"
+#include "product.hpp"
+#include "chain.hpp"
+#include "powerset.hpp"
+#include "range.hpp"
+#include "badkan.hpp"
+
+int main(){
+
+    badkan::TestCase testcase;
+	int grade=0;
+	int signal = setjmp(badkan::longjmp_buffer);
+	if (signal == 0) {
+
+        
+
+    /**
+     * put tests here,
+     * template:
+     * testcase.setname()
+     * .testname
+     * ;
+     */
+
+
+    grade = testcase.grade();
+	} else {
+		testcase.print_signal(signal);
+		grade = 0;
+	}
+	cout << "Your grade is: "  << grade << endl;
+	return 0;
+}
