9c5da76bd7ce4e644a110bc39f8cdb8c4106333a
Michal Yelkin <44944939+michalkin9@users.noreply.github.com>
2019-05-14 17:16:55 +0300

    Add files via upload

--- powerset.hpp
+++ powerset.hpp
@@ -1,1 +1,47 @@
-//powerset
+namespace itertools{
+
+template  <typename T> class powerset{
+
+private:
+T iter1;
+
+public: 
+powerset<T>(T power) : iter1(power){};
+
+//inner class iterator
+class iterator{
+public:
+T powerIter;
+iterator(T a):powerIter(a){}
+
+iterator& operator++()
+{
+    return *this;
+}
+const iterator operator++(int)
+{
+    return *this;
+}
+
+bool operator==(const iterator &other) const
+{
+    return other.powerIter == powerIter;
+}
+bool operator!=(const iterator &other) const
+{
+    return other.powerIter != powerIter;
+}
+
+auto operator*() const
+{
+    return powerIter;
+}
+
+};//end iterator class
+
+public:
+auto begin()const {return iter1.begin();} // return iterator(iterable1, iterable2)
+auto end()const{return iter1.end();} // return iterator(iterable1, iterable2, false)
+};//end class chain
+
+};//end namespace itertools
--- product.hpp
+++ product.hpp
@@ -1,1 +1,49 @@
-//product
+namespace itertools{
+
+template  <typename E,typename M> class product{
+
+private:
+E iter1;
+M iter2;
+
+public: 
+product<E,M>(E start, M end) : iter1(start),iter2(end){};
+
+//inner class iterator
+class iterator{
+public:
+E firstIter;
+M secondIter;
+iterator(E a,M b):firstIter(a),secondIter(b){}
+
+iterator& operator++()
+{
+    return *this;
+}
+const iterator operator++(int)
+{
+    return *this;
+}
+
+bool operator==(const iterator &other) const
+{
+    return other.firstIter == firstIter;
+}
+bool operator!=(const iterator &other) const
+{
+    return other.firstIter != firstIter;
+}
+
+auto operator*() const
+{
+    return firstIter;
+}
+
+};//end iterator class
+
+public:
+auto begin()const {return iter1.begin();} // return iterator(iterable1, iterable2)
+auto end()const{return iter1.end();} // return iterator(iterable1, iterable2, false)
+};//end class chain
+
+};//end namespace itertools
