0b57a67ece99870158b072f8322a496c562d8046
Michal Yelkin <44944939+michalkin9@users.noreply.github.com>
2019-05-25 18:04:28 +0300

    fix product

--- product.hpp
+++ product.hpp
@@ -1,49 +1,112 @@
-namespace itertools{
+#pragma once
+#include <iostream>
+#include <utility>
+#include <ostream>
+using std::pair;
 
-template  <typename E,typename M> class product{
 
-private:
-E iter1;
-M iter2;
-
-public: 
-product<E,M>(E start, M end) : iter1(start),iter2(end){};
-
-//inner class iterator
-class iterator{
-public:
-E firstIter;
-M secondIter;
-iterator(E a,M b):firstIter(a),secondIter(b){}
-
-iterator& operator++()
+namespace itertools
 {
-    return *this;
-}
-const iterator operator++(int)
+template <typename T, typename U>
+class product
 {
-    return *this;
-}
-
-bool operator==(const iterator &other) const
-{
-    return other.firstIter == firstIter;
-}
-bool operator!=(const iterator &other) const
-{
-    return other.firstIter != firstIter;
-}
-
-auto operator*() const
-{
-    return firstIter;
-}
-
-};//end iterator class
 
 public:
-auto begin()const {return iter1.begin();} // return iterator(iterable1, iterable2)
-auto end()const{return iter1.end();} // return iterator(iterable1, iterable2, false)
-};//end class chain
+    const T container1;
+    const U container2;
 
-};//end namespace itertools
+    product(const T &a, const U &b) : container1(a), container2(b) {}
+
+    // inner iterator class
+    class iterator
+    {
+
+    public:
+        decltype(container1.begin()) iter1;
+        decltype(container1.end()) iter1_end;
+        decltype(container2.begin()) iter2;
+        decltype(container2.begin()) iter2Begin;
+        decltype(container2.end()) iter2end;
+        //bool finished;
+
+        iterator(const T &a, const U &b) : 
+        iter1(a.begin()), iter2(b.begin()), iter1_end(a.end()), iter2end(b.end()), iter2Begin(b.begin()) {}
+
+        iterator(const T &a, const U &b, int i) : iter1(a.end()), iter1_end(a.end()), iter2(b.end()) {}
+
+        auto operator*() const
+        {
+
+            return make_pair(*iter1, *iter2);
+        }
+
+        iterator &operator++()
+        {
+           
+            // std::cout << "hi";
+            // if (!(iter2 != iter2end))
+            // {
+            //     std::cout << "as";
+            //     ++iter1;
+            //     iter2=iter2Begin; 
+            // }
+            // else
+            // {
+            //     iter2++;
+            if(iter1 != iter1_end){
+                ++iter2;
+            }
+
+            if(iter2 == iter2end){
+                iter2 = iter2Begin;
+                ++iter1;
+            }
+            // }
+
+            //if(iter1)
+            
+            
+            // else 
+            // {
+            //     iter2 = iter2Begin;
+            //     ++iter1;
+            //     std::cout << "as";
+            // }
+            return *this;
+
+        }
+
+        bool operator!=(const iterator &other)
+        {
+
+            return (iter1 != other.iter1 && iter2 != other.iter2);
+        }
+
+        bool operator==(const iterator &other)
+        {
+
+            return (iter1 == other.iter1 && iter2 == other.iter2);
+        }
+    };
+
+    //end of class iterator
+
+    iterator begin() const
+    {
+        return iterator{container1, container2};
+    }
+    iterator end() const
+    {
+        return iterator{container1, container2, 1};
+    }
+};
+
+// template <typename T, typename U>
+// ostream &operator<<(ostream &os, const pair<T, U> &pair)
+// {
+//     os << pair.first << "," << pair.second;
+//     return os;
+//}
+
+} // namespace itertools
+
